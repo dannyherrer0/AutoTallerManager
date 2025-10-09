1531f172b5e3103c03952ed3f1416bf9a960f8fb
dannyherrer0 <daniherrerarojas5@gmail.com>
2025-10-09 12:08:27 -0500

    feat: :fire: remove files
    removile files

--- Infrastructure/Data/AutoTallerDbContext.cs
+++ Infrastructure/Data/AutoTallerDbContext.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Infrastructure.Persistence
+namespace Infrastructure.Data
 {
-    public class AppDbContext
+    public class AutoTallerDbContext
     {
         
     }
